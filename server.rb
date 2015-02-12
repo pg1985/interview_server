699936bb41b80fc86c6a6fafc5e7e3ca7eef06d3
Patrick Guffey <patrickCguffey@gmail.com>
2015-02-11 21:03:39 -0600

    added a post-route

--- server.rb
+++ server.rb
@@ -9,12 +9,15 @@ get '/:value' do
 end
 
 post '/addUser/:params' do
-
+	unless "#{params[:params]}" == "success"
+		status 400 
+		return "Error"
+	else
+		status 200
+		return "#{params[:params]}"
+	end
 end
 
 def get_response (value)
 	return "You sent " << value
-end
-
-def post_response(data)
 end
